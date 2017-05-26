2d4414334d6ff9897e47d47d1e62688b246c6d7b
CHI-JAN <jan584412@gmail.com>
2017-05-26 17:56:41 +0800

    Go To Activity1

--- app/src/main/java/com/lab453/cyel/githubpractice/Activity7.java
+++ app/src/main/java/com/lab453/cyel/githubpractice/Activity7.java
@@ -1,7 +1,10 @@
 package com.lab453.cyel.githubpractice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 //TODO 請啟展做一個點擊後會跳到com.lab453.cyel.Activity1的Button
 public class Activity7 extends AppCompatActivity {
@@ -10,5 +13,18 @@ public class Activity7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_7);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoAct();
+            }
+        });
+    }
+
+    private void gotoAct(){
+        Intent intent = new Intent(this, Activity1.class);
+        startActivity(intent);
     }
 }
