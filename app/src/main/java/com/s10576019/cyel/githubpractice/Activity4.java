1b0e961b0ba7ab4444becb4a2cb5a7a276007de6
Jim <jim83803@gmail.com>
2017-05-26 14:52:12 +0800

    Initial commit

---
+++ app/src/main/java/com/s10576019/cyel/githubpractice/Activity4.java
@@ -0,0 +1,13 @@
+package com.s10576019.cyel.githubpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_4);
+    }
+}
