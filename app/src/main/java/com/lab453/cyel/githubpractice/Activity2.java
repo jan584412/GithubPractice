b01a2e0eb27dff0b20953e129699031dc31122d0
Jim <jim83803@gmail.com>
2017-05-26 17:19:55 +0800

    開發支線- 第一次分配

--- app/src/main/java/com/lab453/cyel/githubpractice/Activity2.java
+++ app/src/main/java/com/lab453/cyel/githubpractice/Activity2.java
@@ -1,7 +1,9 @@
 package com.lab453.cyel.githubpractice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 //TODO 請劉濬做一個點擊後會跳到com.lab453.cyel.Activity3的Button
 public class Activity2 extends AppCompatActivity {
@@ -9,5 +11,10 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+
+    public void clickButtonJumpToPageYiShuan(View view) {
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
     }
 }
