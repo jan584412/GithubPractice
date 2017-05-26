f2a77bbfd94fc31eb0865efe6907b2814a005880
yishuan <edward0487@gmail.com>
2017-05-26 17:01:23 +0800

    toA4

--- app/src/main/java/com/lab453/cyel/githubpractice/Activity3.java
+++ app/src/main/java/com/lab453/cyel/githubpractice/Activity3.java
@@ -1,7 +1,9 @@
 package com.lab453.cyel.githubpractice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 //TODO 請亦軒做一個點擊後會跳到com.lab453.cyel.Activity4的Button
 public class Activity3 extends AppCompatActivity {
@@ -10,5 +12,11 @@ public class Activity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+    }
+
+    public void goToActivity4(View view) {
+        Intent intent=new Intent();
+        intent.setClass(this,Activity4.class);
+        startActivity(intent);
     }
 }
