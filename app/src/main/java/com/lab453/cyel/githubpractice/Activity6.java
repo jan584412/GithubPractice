420299910a98bb691beca3fe44f916ab00521cc0
sos89937825 <sos8993782542@gmail.com>
2017-05-26 18:29:00 +0800

    hey

--- app/src/main/java/com/lab453/cyel/githubpractice/Activity6.java
+++ app/src/main/java/com/lab453/cyel/githubpractice/Activity6.java
@@ -1,7 +1,10 @@
 package com.lab453.cyel.githubpractice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 //TODO 請進昇做一個點擊後會跳到com.lab453.cyel.Activity7的Button
 public class Activity6 extends AppCompatActivity {
@@ -10,5 +13,17 @@ public class Activity6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_6);
+
+        Button NextPageButton =(Button)findViewById(R.id.button1);
+        NextPageButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setClass(Activity6.this, Activity7.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
