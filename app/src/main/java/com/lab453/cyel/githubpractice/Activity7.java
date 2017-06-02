76d374baaca1dde1b461e49f7215c2451f1fc914
CHI-JAN <jan584412@gmail.com>
2017-06-02 12:57:52 +0800

    加入螢幕翻轉時的螢幕保護功能

--- app/src/main/java/com/lab453/cyel/githubpractice/Activity7.java
+++ app/src/main/java/com/lab453/cyel/githubpractice/Activity7.java
@@ -6,14 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.lab453.cyel.githubpractice.sensors.FlippingManager;
+import com.lab453.cyel.githubpractice.tools.ScreenSaver;
+
 // 2017/05/29 在切換頁面時加入finish()   by 劉濬
+// 2017/06/02 加入手機翻轉時的螢幕保護功能   by啟展
 
 //TODO 請啟展加入手機下翻時，螢幕保護的功能(利用FlippingManager和ScreenSaver)
 //TODO 請啟展將更動寫在前面的註解裡，像是我上面寫的那樣
 public class Activity7 extends AppCompatActivity {
+    FlippingManager flippingManager;
+    ScreenSaver screenSaver;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_7);
 
@@ -22,6 +28,25 @@ public class Activity7 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 gotoAct();
+            }
+        });
+
+        if (flippingManager == null) {
+            flippingManager = new FlippingManager(this);
+        }
+        if (screenSaver == null) {
+            screenSaver = new ScreenSaver(this);
+        }
+
+        flippingManager.start(new FlippingManager.OnScreenFlipListener() {
+            @Override
+            public void onScreenFlipped(int facingDirection) {
+                if (facingDirection == DIRECTION_DOWN) {
+                    screenSaver.startScreenSaving();
+                }
+                if (facingDirection == DIRECTION_UP) {
+                    screenSaver.stopScreenSaving();
+                }
             }
         });
     }
